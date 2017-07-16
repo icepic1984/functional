30206b3221558f8c0be90a1007db7de0af3b2a3e
icepic <icepic@mapsource.fritz.box>
2017-07-16 20:13:43 +0200

    Add exercise 3.1

--- list.scala
+++ list.scala
@@ -12,9 +12,22 @@ object List1 {
     else Cons(as.head, apply(as.tail: _*))
   }
 
+  def tail[A]( a : List1[A]) :  List1[A] = a match {
+    case Cons(_, xs) => xs
+  }
+
+
   def main(args : Array[String]) : Unit =
   {
     List1(10, 20)
+    val x = List1(1,2,3,4,5) match {
+      case Cons(x, Cons(2,Cons(4,_))) => x
+      case Nil => 42
+      case Cons( x, Cons(y,Cons(3, Cons(4,_)))) => x + y
+      case Cons(h, t) =>  h + sum(t)
+      case _ => 101
+    }
+    println(x)
   } 
 
 }
